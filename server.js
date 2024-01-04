f40ec3f2f105b4ca59f4a69a73c355aedbcedd04
piyawat pintusornsri <piyawat5930302600@gmail.com>
2024-01-04 14:48:40 +0700

    fix cors

--- server.js
+++ server.js
@@ -4,18 +4,17 @@ const routes = require("./routes/routes");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./config/swagger");
-const allowCrossDomain = function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-};
 
 //Middlewares
 app.use(express.json({ limit: "100000mb" }));
 app.use(express.urlencoded({ limit: "100000mb" }));
-app.use(allowCrossDomain);
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+// app.use(cors());
 
 //routes
 app.use(routes);
