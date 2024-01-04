d8f9109b781db4e84cf65a47320907382eae37ab
piyawat pintusornsri <piyawat5930302600@gmail.com>
2024-01-04 14:42:59 +0700

    allow cross domain

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const routes = require("./routes/routes");
 const app = express();
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./config/swagger");
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
 
 //Middlewares
 app.use(express.json({ limit: "100000mb" }));
 app.use(express.urlencoded({ limit: "100000mb" }));
+app.use(allowCrossDomain);
 app.use(cors());
 
 //routes
